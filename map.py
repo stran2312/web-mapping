22bfea66a7b2a5a1208736b83a5b31c62c2b20db
Sang Tran <100749058+stran2312@users.noreply.github.com>
2022-06-08 01:24:33 -0500

    Add files via upload

--- map.py
+++ map.py
@@ -9,14 +9,27 @@ elev = list(data["ELEV"])
 html = """<h4>Volcano information:</h4>
 Height: %s m
 """
+
+def color_generator(elev):
+    if elev < 1000: return 'green'
+    elif 1000 <= elev < 3000: return 'orange'
+    else: return 'red'
  
 map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name = "My Map")
+fgv = folium.FeatureGroup(name = "Volcanoes")
  
 for lt, ln, el in zip(lat, lon, elev):
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
- 
- 
-map.add_child(fg)
+    fgv.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = color_generator(el))))
+
+fgp = folium.FeatureGroup(name = "Population")
+# fill the marker color corresponding to the location's population
+# POP < 10 000 000 - green / 10 000 000 < POP < 20 000 000 - orange/ anything else is red
+fgp.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig'.read(),
+                                       style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                                       else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map_html_popup_simple.html")
